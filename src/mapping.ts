a425c6482f0ff314bf110afecdf4435870ab8024
cryptodapp <96749852+kenjicncr@users.noreply.github.com>
2022-02-24 21:07:49 -0500

    chore: thissit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,92 +1,120 @@
-import { Bytes, BigInt, log, store } from '@graphprotocol/graph-ts'
-import { StorePixelsCall } from './types/MOVRPlace/MOVRPlaceABI'
+import { Bytes, BigInt, log, store, ethereum } from '@graphprotocol/graph-ts'
+import { StorePixelsCall, StorePixelsCallPixelInputsStruct } from './types/MOVRPlace/MOVRPlaceABI'
 import { Bucket, Account, Charity } from './types/schema'
 
 // globals 
-let pixelArr: Array<BigInt>
+let pixelArr: Array<i32>
 // let bucket: Bucket | null
-let bucketMapping = new Map<string, Array<BigInt>>()
+let bucketMapping = new Map<string, Array<i32>>()
 
-let oldPixelArr: Array<BigInt>
-let newPixelArr: Array<BigInt>
-let combinedPixelArr: Array<BigInt>
+let oldPixelArr: Array<i32>
+let newPixelArr: Array<i32> 
+let combinedPixelArr: Array<i32>
 let blockNumber: BigInt
+let bucketLength: i32
+let _pixelInput: StorePixelsCallPixelInputsStruct
 
 export function handleStorePixels(call: StorePixelsCall): void {
+  let pixelInputs = call.inputs.pixelInputs
+  let totalPixelInputs = call.inputs.pixelInputs.length
 
-  let id = call.transaction.hash.toHex()
-  blockNumber = call.block.number
+  const bucketLength = 16;
 
-  let pixelInputs = new Array<Map<string,BigInt>>()
+  for(let i = 0; i < totalPixelInputs; i++) {
+    const _pixelInput = pixelInputs[i]
+    const posInBucket: i32 = _pixelInput.posInBucket.toI32()
+    const color = _pixelInput.color
+    const bucketIdString = _pixelInput.bucket.toString()
+    const bucketItem: Array<i32> | null = bucketMapping.has(bucketIdString) ? bucketMapping.get(bucketIdString) : []
 
-  for (let step = 0; step < call.inputs.pixelInputs.length; step++) {
-    // Runs 5 times, with values of step 0 through 4.
-
-    const _pixelInput = call.inputs.pixelInputs[0]
-
-    let pixelInputObject = new Map<string,BigInt>()
-
-    pixelInputObject.set("bucket", _pixelInput.bucket)
-    pixelInputObject.set("posInBucket", _pixelInput.posInBucket)
-    pixelInputObject.set("color", new BigInt(_pixelInput.color))
-
-    pixelInputs.push(pixelInputObject)
-  }
-
-  let bucketLength = 16
-
-  for(let i = 0; i < pixelInputs.length; i++) {
-    let _pixelInput = pixelInputs[i]
-    let bucketId = _pixelInput.get("bucket").toString()
-    let posInBucket = parseInt(pixelInputs[i].get("posInBucket").toString()) as i32
-    const color = pixelInputs[i].get("color")
-
-    if (bucketMapping.get(bucketId) == null || bucketMapping.get(bucketId).length <= 0) {
-      let arr = new Array<BigInt>(bucketLength).fill(new BigInt(0))
-
-      bucketMapping.set(bucketId, arr)
-      bucketMapping.get(bucketId)[posInBucket] = color
+    if (!bucketItem || bucketItem.length <= 0) {
+      let newArr: Array<i32> =  new Array<i32>(bucketLength).fill(0)
+      newArr[posInBucket] = color
+      bucketMapping.set(bucketIdString, newArr)
     }
   }
 
+  for(let i = 0; i < totalPixelInputs; i++) {
+    const _pixelInput = pixelInputs[i]
+    const posInBucket: i32 = _pixelInput.posInBucket.toI32()
+    const color = _pixelInput.color
+    const bucketId = _pixelInput.bucket.toString()
+    const emptyBucketPixels: Array<i32> =  new Array<i32>(bucketLength).fill(0)
+    const bucketPixels: Array<i32>  = bucketMapping.has(bucketId) ? bucketMapping.get(bucketId) : emptyBucketPixels
+    const lastBlockUpdated = call.block.number.toI32()
 
-  pixelInputs.forEach((pixelInput) => {
-    const bucketId = pixelInput.get("bucket").toString()
 
     let bucket = Bucket.load(bucketId)
 
     if(!bucket) {
-      let bucket = new Bucket(bucketId)
-      bucket.position = pixelInput.get("bucket")
-      pixelArr = bucketMapping.get(bucketId)
-      bucket.pixels = pixelArr
-      bucket.lastBlockUpdated = blockNumber
+      bucket = new Bucket(bucketId)
+      bucket.position = posInBucket
+      bucket.pixels = bucketPixels
+      bucket.lastBlockUpdated = lastBlockUpdated
 
       bucket.save()
-    }
+    } else {
+      // bucket exists
+      
+      let oldPixels = bucket.pixels
+      let newPixels = bucketPixels
 
-    if(bucket) {
+      let combinedPixels: Array<i32> =  new Array<i32>(bucketLength).fill(0)
 
-      oldPixelArr = bucket.pixels
-      newPixelArr = bucketMapping.get(bucketId)
-
-      for(let i = 0; i < 16; i++) {
-        const newPixel = newPixelArr[i]
+      for(let i = 0; i < bucketLength; i++) {
+        const newPixel = newPixels[i]
 
         // if position is 0, it means no need to override
         // and save the old one
-        if(newPixel === new BigInt(0)) {
-          combinedPixelArr[i] = oldPixelArr[i]
+        if(newPixel === 0) {
+          combinedPixels[i] = oldPixels[i]
         } else {
-          combinedPixelArr[i] = newPixelArr[i]
+          combinedPixels[i] = newPixels[i]
         }
       }
 
-      bucket.pixels = combinedPixelArr
-      bucket.lastBlockUpdated = blockNumber
-
-      log.info("UPDATED BUCKET #{}", [bucketId])
+      bucket.pixels = combinedPixels
+      
+      // update block #
+      bucket.lastBlockUpdated = lastBlockUpdated
+      
       bucket.save()
     }
-  })
+  }
+  
+
+  // Add accounts
+
+  let accountAddress = call.from
+  let accountId = accountAddress.toHexString()
+
+  let account = Account.load(accountId)
+
+  if(!account) {
+    account = new Account(accountId)
+    account.address = accountAddress
+    account.totalPixelsPlaced = totalPixelInputs
+    log.info("New item", [account.totalPixelsPlaced.toString()])
+    account.save()
+  } else {
+    let newTotal =  totalPixelInputs + account.totalPixelsPlaced
+    account.totalPixelsPlaced = newTotal
+
+    account.save()
+  }
+
+  // new charity
+  let charityId = call.inputs._id
+
+  let charity = Charity.load(charityId)
+
+  if(!charity) {
+    charity = new Charity(charityId)
+    charity.name = charityId
+    charity.totalPixels = totalPixelInputs
+    charity.save()
+  } else {
+    charity.totalPixels = charity.totalPixels + totalPixelInputs
+    charity.save()
+  }
 }
